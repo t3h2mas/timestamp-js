e9dc080e65e1d648a12b97f1a3537011705325e5
Thomas <jtruant@gmx.com>
2016-02-19 18:51:14 -0700

    index.html; moment.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => res.end("Hello World!") );
+app.use(express.static(__dirname + '/view'));
+
+app.get('/:date', (req, res) => {
+  if (req.params.date) {
+    var resp = {unix: null, natural: null};
+    res.json(resp);
+    //res.json({"date": req.params.date});
+  }
+  else
+    res.sendFile("index.html");
+});
 
 app.listen(1337, function () {
   console.log('Example app listening on port 1337!');
