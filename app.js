01db2159363d7e1cca1ab752775f4b212e35d72c
Thomas <jtruant@gmx.com>
2016-02-19 19:09:26 -0700

    IT WORKS

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var app = express();
 
+var moment = require('moment');
+
 app.use(express.static(__dirname + '/view'));
+
+function parseDate(date) {
+  var resp = {unix: null, natural: null};
+  var date;
+
+  if (isNaN(date)) {
+    date = moment(date);
+    console.log("Natural: " + date.format());
+    resp.natural = date.format("MMMM D, YYYY");
+    resp.unix = date.format("X");
+  } else {
+    // date should be unix
+    // TODO: Error handling
+    date = moment.unix(date);
+    console.log("Unix: " + date.format());
+    resp.unix = Number(date);
+    resp.natural = date.format("MMMM D, YYYY");
+  }
+  return resp;
+}
 
 app.get('/:date', (req, res) => {
   if (req.params.date) {
-    var resp = {unix: null, natural: null};
+    var resp = parseDate(req.params.date);
     res.json(resp);
     //res.json({"date": req.params.date});
   }
